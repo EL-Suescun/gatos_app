aba92ef71ef382ca9a3f2952b02d832fc8ab7a33
David Giraldo <santiagogiraldosuescun@gmail.com>
2023-06-08 18:17:15 -0500

    Traemos la imagen y nos a seguramos que la imagen este bien dimencionada

--- src/CLASES/GatosService.java
+++ src/CLASES/GatosService.java
@@ -1,6 +1,11 @@
 package CLASES;
 
+import com.google.gson.Gson;
+import java.awt.Image;
 import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import okhttp3.*;
 
 public class GatosService {
@@ -16,12 +21,35 @@ public class GatosService {
                 .method("GET", body)
                 .build();
         Response response = client.newCall(request).execute();
-        
+
         String elJson = response.body().string();
-        
+
         //cortar los corchetes
         elJson = elJson.substring(1, elJson.length());
-        elJson = elJson.substring(0, elJson.length()-1);
-        
+        elJson = elJson.substring(0, elJson.length() - 1);
+
+        //Crear un objeto de la clase Gson
+        Gson gson = new Gson();
+        Gatos gatos = gson.fromJson(elJson, Gatos.class);
+
+        //Redimensionar en caso de necesitar
+        Image image = null;
+        try {
+            URL url = new URL(gatos.getUrl());
+            image = ImageIO.read(url);
+
+            ImageIcon fondoGato = new ImageIcon(image);
+
+            if (fondoGato.getIconWidth() > 800) {
+                //redimensionamos
+                Image fondo = fondoGato.getImage();
+                Image modificada = fondo.getScaledInstance(800, 600, java.awt.Image.SCALE_SMOOTH);
+                fondoGato = new ImageIcon(modificada);
+            }
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
     }
 }
