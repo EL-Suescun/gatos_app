492d525cee81d8dc37a1a4825c5a0fee29ce81f3
David Giraldo <santiagogiraldosuescun@gmail.com>
2023-06-08 17:17:11 -0500

    Creamos las clases inicio y gatos

---
+++ src/CLASES/Gatos.java
@@ -0,0 +1,43 @@
+
+package CLASES;
+
+public class Gatos {
+    int id;
+    String url;
+    String apikey = "live_jS2kZ0YdDdh9LU4Zfkkx1c3e4cSOicnWwIKRpjh3XZDFAk9YURtj8b6Z3Wyg4mE0";
+    String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+    
+}
---
+++ src/CLASES/Inicio.java
@@ -0,0 +1,32 @@
+package CLASES;
+
+import javax.swing.JOptionPane;
+
+public class Inicio {
+
+    public static void main(String[] args) {
+        int opcion_menu = -1;
+        String[] botones = {"1. Ver gatos", "2. Salir"};
+
+        do {
+            //Menu principal
+            String opcion = (String) JOptionPane.showInputDialog(null, "Gatitos Java", "Menu Principal",
+                    JOptionPane.INFORMATION_MESSAGE, null, botones, botones[0]);
+
+            //Validamos que opcion selecciono el usuario
+            for (int i = 0; i < botones.length; i++) {
+                if (opcion.equals(botones[i])) {
+                    opcion_menu = i;
+                }
+            }
+            switch (opcion_menu) {
+                case 0:
+
+                    break;
+                default:
+
+                    break;
+            }
+        } while (opcion_menu != 1);
+    }
+}
