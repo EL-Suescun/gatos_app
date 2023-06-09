24b9b93f44383954c6a84b33f8b968ddbb0009f6
David Giraldo <santiagogiraldosuescun@gmail.com>
2023-06-08 21:08:51 -0500

    No se como decirlo, solo estamos haciendo algo para mostrar los gatos favoritos pero con mas clases

---
+++ src/CLASES/gatosFav.java
@@ -0,0 +1,41 @@
+package CLASES;
+public class gatosFav {
+    String id;
+    String image_id;
+    String apikey = "live_jS2kZ0YdDdh9LU4Zfkkx1c3e4cSOicnWwIKRpjh3XZDFAk9YURtj8b6Z3Wyg4mE0";
+    Imagex image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage_id() {
+        return image_id;
+    }
+
+    public void setImage_id(String image_id) {
+        this.image_id = image_id;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public Imagex getImage() {
+        return image;
+    }
+
+    public void setImage(Imagex image) {
+        this.image = image;
+    }
+    
+    
+}
